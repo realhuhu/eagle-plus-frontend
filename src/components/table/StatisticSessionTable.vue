1f0091479349ec0095a29be27c4679085d5b7578
realhuhu <2661467107@qq.com>
2024-04-15 08:59:55 +0800

    home

---
+++ src/components/table/StatisticSessionTable.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    123
+  </div>
+</template>
+
+<script setup lang="ts">
+import { client } from '@/assets/lib/request';
+import { ref } from 'vue';
+import { Notification } from "@arco-design/web-vue";
+
+const loading = ref(false)
+
+const get_data = async () => {
+  loading.value = true
+  try {
+    const res = await client.get<StaticSessionPoint[]>({
+      url: "/statistic/session"
+    })
+
+    console.log(res.data)
+  } catch (e) {
+    Notification.warning("获取失败")
+    console.error(e)
+  } finally {
+    loading.value = false
+  }
+}
+
+get_data()
+</script>
+
+<style scoped lang="less"></style>
