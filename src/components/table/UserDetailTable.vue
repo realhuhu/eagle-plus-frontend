473964bc7bba8551cc9aa79a35263262b55495a1
realhuhu <2661467107@qq.com>
2024-06-16 01:21:43 +0800

    icon

--- src/components/table/UserDetailTable.vue
+++ src/components/table/UserDetailTable.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
     <a-button type="text" class="float-right" @click="safeBack('/user/all')">返回</a-button>
+    <div class="w-full">
+      <div v-if="!loading" class="flex flex-col justify-start items-center gap-4 w-full">
+        <user-info-card :uid="uid"/>
+        <user-word-cloud-card :uid="uid"/>
+        <user-interaction-card :uid="uid"/>
+        <user-award-card :uid="uid"/>
+      </div>
 
-    <div class="flex flex-col justify-start items-center gap-4 w-full">
-      <user-info-card :uid="uid"/>
-      <user-word-cloud-card :uid="uid"/>
-      <user-interaction-card :uid="uid"/>
-      <user-award-card :uid="uid"/>
+      <a-spin class="w-full" v-else tip="加载中"/>
     </div>
   </div>
 </template>
@@ -14,15 +17,34 @@
 <script setup lang="ts">
 import {ref, watch} from "vue";
 import {useRoute} from "vue-router";
+import {Notification} from "@arco-design/web-vue";
 import {safeBack} from "@/router";
 import {flat_query} from "@/assets/lib/utils";
+import {client} from "@/assets/lib/request";
 
 const route = useRoute()
 const uid = ref(Number(flat_query(route.params.uid)))
+const loading = ref(true)
+
+const get_data = async () => {
+  loading.value = true
+  try {
+    const res = await client.get<User>({url: `/user/${uid.value}`})
+    if (res.msg === "error") {
+      Notification.warning("用户不存在")
+      safeBack("/user/top")
+    }
+  } finally {
+    loading.value = false
+  }
+}
+
 
 watch(() => route.params.uid, () => {
   uid.value = Number(flat_query(route.params.uid))
 })
+
+get_data()
 </script>
 
 <style scoped lang="less"></style>
