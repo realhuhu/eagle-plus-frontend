7246b7c21ae53bbbb59f27d40949454c1fbdc278
realhuhu <2661467107@qq.com>
2024-07-13 17:27:00 +0800

    url import

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
+import fs from "fs";
 import {resolve} from "path";
 import {defineConfig} from "vite"
+import cheerio from "cheerio";
+import {type OutputBundle, type OutputAsset, type OutputChunk} from "rollup"
 import Vue from "@vitejs/plugin-vue"
 import Icons from "unplugin-icons/vite"
 import Components from "unplugin-vue-components/vite"
@@ -8,6 +11,44 @@ import IconsResolver from "unplugin-icons/resolver"
 import {ArcoResolver} from "unplugin-vue-components/resolvers";
 import viteCompression from "vite-plugin-compression";
 import {vitePluginForArco} from "@arco-plugins/vite-vue";
+
+
+function URLImport(url: string) {
+  return {
+    name: "url-import",
+    writeBundle(_, bundle: OutputBundle) {
+      const html = bundle["index.html"] as OutputAsset
+      const srcValues: string[] = []
+
+      const $ = cheerio.load(html.source.toString())
+      $("[src]").each(function () {
+        const src = $(this).attr("src")
+        if (src) {
+          $(this).attr("src", url + src)
+          srcValues.push(src)
+        }
+      })
+      $("[href]").each(function () {
+        const href = $(this).attr("href")
+        if (href) {
+          $(this).attr("href", url + href)
+        }
+      });
+
+      fs.writeFileSync("dist/index.html", $.html())
+
+      srcValues.forEach(src => {
+        const chunk = bundle[src.slice(1)] as OutputChunk
+        chunk.code = chunk.code.replace(
+          /import\("\.\/(.+?\.js)"\)/g,
+          (_, p) => `import("${url}/assets/${p}")`
+        )
+        chunk.code = chunk.code.replace("r.href=e", `r.href="${url}"+e`)
+        fs.writeFileSync(`dist/${chunk.fileName}`, chunk.code)
+      })
+    }
+  };
+}
 
 export default defineConfig({
   plugins: [
@@ -34,6 +75,18 @@ export default defineConfig({
       resolvers: [ArcoResolver(), IconsResolver()]
     })
   ],
+  build: {
+    rollupOptions: {
+      plugins: [URLImport("https://dist.realhuhu.com/iying")]
+    },
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": resolve(__dirname, "src")
